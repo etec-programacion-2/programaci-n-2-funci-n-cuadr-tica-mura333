649ff8c304be0e5fe1bad6b379e46f350f4374af
mura33 <ms.mura@alumno.etec.um.edu.ar>
2025-05-09 10:25:44 -0300

    calculo de raices de la funcion

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,35 +1,27 @@
 
-package org.example
 
 
-fun main() {
-    val a = 2
-    val b = 5
-    val c = -3    
-    
-        if (a == 0) {
-        println("El coeficiente 'a' no puede ser cero.")
-        return
-    }    
-    
-    val funcion = buildString {
-        append("${a}x² ")
-        append(if (b >= 0) "+ $b" else "- ${-b}")
-        append("x ")
-        append(if (c >= 0) "+ $c" else "- ${-c}")
+fun calcularRaices(a: Double, b: Double, c: Double) {
+    val discriminante = b * b - 4 * a * c
+
+    if (discriminante > 0) {
+        val raiz1 = (-b + Math.sqrt(discriminante)) / (2 * a)
+        val raiz2 = (-b - Math.sqrt(discriminante)) / (2 * a)
+        println("Raíces reales distintas: %.2f y %.2f".format(raiz1, raiz2))
+    } else if (discriminante == 0.0) {
+        val raiz = -b / (2 * a)
+        println("Raíz doble: %.2f".format(raiz))
+    } else {
+        val parteReal = -b / (2 * a)
+        val parteImaginaria = Math.sqrt(-discriminante) / (2 * a)
+        println("Raíces complejas: %.2f + %.2fi y %.2f - %.2fi".format(parteReal, parteImaginaria, parteReal, parteImaginaria))
     }
 }
+
 fun main() {
-    val a = 2
-    val b = -4
-    val c = 1
-
-    println("Tabla de valores de f(x) = ${a}x² + (${b})x + (${c}):")
-    println("x\t|\tf(x)")
-    println("-----------------")
-
-    for (x in -5..5) {
-        val fx = a * x * x + b * x + c
-        println("$x\t|\t$fx")
-    }
+    val a = 1.0
+    val b = -3.0
+    val c = 2.0
+    calcularRaices(a, b, c)
 }
+
